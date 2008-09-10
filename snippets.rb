3df1c5ae227c624c74ee49c964856fd5b8b963fa
Benoit Pierre <benoit.pierre@gmail.com>
2008-09-11 00:29:05 +0200

    Better C snippets.

--- snippets.rb
+++ snippets.rb
@@ -1,56 +1,65 @@
 #! /usr/bin/env ruby
 
-expand 'keyword ' do
+# C
 
-  defsnippet 'main', :c, :cpp, %q[
-int main(int argc, char *argv[])
-{
-<++>^^^
-return 0;
-}
-  ]
+expand "keyword\t" do
 
-  defsnippet 'for', :c, :cpp, %q[
+  defsnippet 'for ', :c, :cpp, %q[
 for (<+init+>; <+cond+>; <+step+>)
 {
-<++>
+<+code+>
 }
   ]
 
-  defsnippet 'while', :c, :cpp, %q[
+  defsnippet 'while ', :c, :cpp, %q[
 while (<+cond+>)
 {
-<++>
+<+code+>
 }
   ]
 
-  defsnippet 'while', :c, :cpp, %q[
-while (<+cond+>)
-{
-<++>
-}
-  ]
-
-  defsnippet 'switch', :c, :cpp, %q[
+  defsnippet 'switch ', :c, :cpp, %q[
 switch (<+value+>)
 {
 case <+value+>:
-<++>^^^
+<+code+>^^^
 break;
 
 default:
-<++>^^^
+<+code+>^^^
 break;
 }
   ]
 
-  defsnippet 'case', :c, :cpp, %q[
+  defsnippet 'case ', :c, :cpp, %q[
 case <+value+>:
-<++>^^^
+<+code+>^^^
 break;
   ]
 
+end
 
+expand 'keyword<' do
+
+  defsnippet '#include', :cpp, %q[
+    #include <<+header+>>
+  ]
+
+  defsnippet '#inc', :c, :cpp, %q[
+    #include <<+header+>>
+  ]
+
+end
+
+expand 'keyword"' do
+
+  defsnippet '#include', :c, :cpp, %q[
+    #include \"<+header+>\"
+  ]
+
+  defsnippet '#inc', :c, :cpp, %q[
+    #include \"<+header+>\"
+  ]
 
 end
 
