9bb1b70f8a04376cc89038607992b1624b63e45c
Benoit Pierre <benoit.pierre@gmail.com>
2008-09-16 00:08:44 +0200

    Convert leftover tSkeleton templates to yasnippets format.

--- skeletons/templates/cpp #.hh
+++
@@ -1,8 +0,0 @@
-<+let(b:uname=toupper(substitute(fnamemodify(expand('%'), ':t'), '[^A-Za-z_0-9]', '_', 'g')))+>
-
-#ifndef <+b:uname+>
-# define <+b:uname+>
-
-<+CURSOR+>
-
-#endif /* !<+b:uname+> */
