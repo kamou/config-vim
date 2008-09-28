b2e3f7e2592650b580b772b46c88fd266b4f7020
Benoit Pierre <benoit.pierre@gmail.com>
2008-09-29 00:24:47 +0200

    Actually make sure general skeleton snippets are available for all files.

--- snippets.rb
+++ snippets.rb
@@ -1,5 +1,45 @@
 #! /usr/bin/env ruby
 
+require 'find'
+
+def load_skeleton_snippets(directory, *filetypes)
+
+  expand "keyword\t" do
+
+    skel_dir = VIM::evaluate('g:yasnippets_skeletons')
+
+    begin
+
+      cwd = Dir.pwd
+
+      Dir.chdir(skel_dir)
+
+      Find.find(directory) { |skel|
+
+        next if File.directory?(skel)
+        next unless File.readable?(skel)
+
+        #VIM::message(skel)
+
+        args = [ "<%=#{skel}>" ]
+        args.concat(filetypes)
+        args << File.read(skel)
+
+        defsnippet(*args)
+      }
+
+    ensure
+
+      Dir.chdir(cwd)
+
+    end if skel_dir
+
+  end
+
+end
+
+load_skeleton_snippets('general', :all)
+
 Dir.glob(File.join(ENV['USERVIM'], 'snippets', '*.rb')) { |entry|
 
   load(entry)
--- snippets/c,cpp.rb
+++ snippets/c,cpp.rb
@@ -1,7 +1,5 @@
 #! /usr/bin/env ruby
 
-require 'find'
-
 expand "keyword\t" do
 
   defsnippet 'for ', :c, :cpp, %q[
@@ -37,31 +35,9 @@ case <+value+>:
 break;
   ]
 
-  skel_dir = VIM::evaluate('g:yasnippets_skeletons')
-
-  begin
-
-    cwd = Dir.pwd
-
-    Dir.chdir(skel_dir)
-
-    Find.find('general', 'c') { |skel|
-
-      next if File.directory?(skel)
-      next unless File.readable?(skel)
-
-      #VIM::message(skel)
-
-      defsnippet "<%=#{skel}>", :c, :cpp, File.read(skel)
-    }
-
-  ensure
-
-    Dir.chdir(cwd)
-
-  end if skel_dir
-
 end
+
+load_skeleton_snippets('c', :c, :cpp)
 
 expand 'keyword<' do
 
