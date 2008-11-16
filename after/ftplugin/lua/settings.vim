239501d5bcd4cf9c7c7d717009944194ee44a21e
Benoit Pierre <benoit.pierre@gmail.com>
2008-11-16 04:38:03 +0100

    Add LUA settings, dictionary, and snippets.

---
+++ after/ftplugin/lua/settings.vim
@@ -0,0 +1,12 @@
+
+" Correct indent level, tab replaced by 2 spaces.
+setlocal expandtab
+setlocal shiftwidth=2
+setlocal softtabstop=2
+
+" Highlight trailing white spaces, tabs.
+setlocal list
+
+" Dictionary for completion.
+setlocal dictionary+=$USERVIM/dictionaries/lua.dic
+
---
+++ snippets/lua.rb
@@ -0,0 +1,74 @@
+#! /usr/bin/env ruby
+
+expand "keyword\t" do
+
+  defsnippet 'do ', :lua, %q[
+do
+<+block+>
+end
+  ]
+
+  defsnippet 'while ', :lua, %q[
+while <+exp+> do
+<+block+>
+end
+  ]
+
+  defsnippet 'repeat ', :lua, %q[
+repeat
+<+block+>
+until <+exp+>
+  ]
+
+  defsnippet 'if ', :lua, %q[
+if <+exp+> then
+<+block+>
+end
+  ]
+
+  defsnippet 'ifelse ', :lua, %q[
+if <+exp+> then
+<+block+>
+else
+<+block+>
+end
+  ]
+
+  defsnippet 'elseif ', :lua, %q[
+elseif <+exp+> then
+<+block+>
+end
+  ]
+
+  defsnippet 'for ', :lua, %q[
+for <+namelist+> in <+explist+> do
+<+block+>
+end
+  ]
+
+  defsnippet 'function ', :lua, %q[
+function <+name+> (<+parlist+>)
+<+body+>
+end
+  ]
+
+  defsnippet 'function)', :lua, %q[
+function <+name+> ()
+<+body+>
+end
+  ]
+
+  defsnippet 'function(', :lua, %q[
+function (<+parlist+>)
+<+body+>
+end
+  ]
+
+  defsnippet 'function()', :lua, %q[
+function ()
+<+body+>
+end
+  ]
+
+end
+
